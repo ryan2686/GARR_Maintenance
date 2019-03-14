657a52de530b07c77e7502e38d891d0a414dfedd
French <RFrench@bentallkennedy.com>
2019-03-14 12:57:29 -0700

    Updated Comments

--- GARR_HASH.sql
+++ GARR_HASH.sql
@@ -146,3 +146,37 @@ FROM
 	) S2
 	GROUP BY GROUPID
 ) S3
+
+
+
+-- The below code works but due to limitations of current SQL Server version, HASHBYTES function
+--	is limited to 8000 bytes.
+--		In the next iteration I will strip the BK from the acctnum and attempt some data normalization
+--		similar to what would be applied in deep learning.ABORT
+-- I would also like to add an instance counter but that would require more subqueries
+SELECT DISTINCT S3.GROUPID, HASHBYTES('SHA1', CAST(AcctList AS VarBinary(7999))) As HashValue
+FROM 
+(
+	SELECT 	GROUPID, 
+			STUFF((	SELECT ',' + S1.ACCTNUM
+					FROM 
+						(
+							SELECT DISTINCT GR.GROUPID, GC.ACCTNUM
+							FROM BKUSLP.BKUSLP.dbo.GACC GC, BKUSLP.BKUSLP.dbo.GARR GR
+							WHERE GR.LEDGCODE = 'BK'
+							AND (
+								(GC.ACCTNUM >= GR.BEGACCT AND GC.ACCTNUM <= GR.ENDACCT)
+	    							)
+						) S1
+					WHERE S1.GROUPID=S2.GROUPID FOR XML PATH('')),1,1,'') As AcctList
+	FROM
+	(
+		SELECT GR.GROUPID, GC.ACCTNUM
+		FROM BKUSLP.BKUSLP.dbo.GACC GC, BKUSLP.BKUSLP.dbo.GARR GR
+		WHERE GR.LEDGCODE = 'BK'
+			AND (
+				(GC.ACCTNUM >= GR.BEGACCT AND GC.ACCTNUM <= GR.ENDACCT)
+	    			)
+	) S2
+	GROUP BY GROUPID
+) S3
